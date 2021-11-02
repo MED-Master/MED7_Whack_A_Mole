5616e1274a62e9cbeb9a7a7038e3393b1c3acc18
SteffenLehmann <slehma15@student.aau.dk>
2021-11-02 13:56:24 +0100

    Add: Wind sound, fixed the flood, toggle for VE and fixed for animation speeds

---
+++ Assets/Scripts/UI/VE_Toggle.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VE_Toggle : MonoBehaviour
+{
+    public GameObject VE;
+    // Start is called before the first frame update
+    void Start()
+    {
+        VE.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.V))
+        {
+            if (VE.activeSelf == true)
+            {
+                VE.SetActive(false);
+            }
+            else
+            {
+                VE.SetActive(true);
+            }
+        }
+    }
+}
